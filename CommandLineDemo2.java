82c939e7bb73d2a8ccc6a48a15e9fbc1cf8dfc9b
Manvitha-2004 <245122748051@mvsrec.edu.in>
2025-04-01 12:04:22 +0530

    Add files via upload

---
+++ CommandLineDemo2.java
@@ -0,0 +1,14 @@
+class CommandLineDemo2
+{
+public static void main(String args[])
+{
+String s1=args[0];
+String s2=args[1];
+String s3=args[2];
+
+System.out.println("name:"+s1);
+System.out.println("roll no:"+s2);
+System.out.println("dept:"+s3);
+
+}
+}
---
+++ CommandLineDemo5.py
@@ -0,0 +1,8 @@
+import sys
+s1=sys.argv[1]
+s2=sys.argv[2]
+s3=sys.argv[3]
+print("name",s1)
+print("roll",s2)
+print("dept",s3)
+print("file",sys.argv[0])
